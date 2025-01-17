a7404d1eb9624eb75b514333bf138cd2c805717a
Anyaele <kirchoff200@gmail.com>
2025-01-17 20:08:02 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js"
+import { connectDB } from "./config/db.js";
+import cors from "cors"
+
+dotenv.config()
+
+// initialize express
+const app = express();
+
+
+//connect port
+const PORT = process.env.PORT || 8080;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: ["http://localhost:8080", ""],
+}));
+app.use("/api/users", userRoutes)
+
+
+
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+})
